79d5d622d36b5ef499c0593899171a06568ae3f8
Anthony Lupu <LupuLupuLupuLupuLupuLupu@gmail.com>
2020-02-26 19:09:58 -0500

    weak 3 dog

---
+++ weweeeeeeekekekekekewewewkwekewkwkewekkwek 3/weweeeeeeekekekekekewewewkwekewkwkewekkwek 3/dog_dog_dog_dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace weweeeeeeekekekekekewewewkwekewkwkewekkwek_3
+{
+    class dog_dog_dog_dog
+    {
+        public string no;
+        public int weight;
+        public string naso;
+        public void Björk()
+        {
+            MessageBox.Show("björk");
+        }
+        public void SayName()
+        {
+            MessageBox.Show("me name " + naso);
+        }
+    }
+}
